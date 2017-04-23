7935bb11413eaef3294139b956d09f68c3d3c2f0
tchi <tchi@riotgames.com>
2017-04-23 16:43:13 +0900

    Modify game to manage the current players by provided player list

--- src/main/java/univ/cnu/lecture/gostop/Game.java
+++ src/main/java/univ/cnu/lecture/gostop/Game.java
@@ -4,8 +4,18 @@ package univ.cnu.lecture.gostop;
  * Created by tchi on 2017. 4. 23..
  */
 public class Game {
+    private Player[] players;
+    private int currentPlayerIndex;
+
+    public Game(Player[] players) {
+        this.players = players;
+        this.currentPlayerIndex = 0;
+    }
+
     public Player currentPlayer() {
-        return null;
+        int index = currentPlayerIndex;
+        currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
+        return players[index];
     }
 
     public Card[] putCard(Card playerCard) {
