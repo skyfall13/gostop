0d1cc4e672b824cd78ece035cf3cf1609cf23a9d
tchi <tchi@riotgames.com>
2017-04-23 16:28:15 +0900

    Not existing implementation test

---
+++ src/test/java/univ/cnu/lecture/gostop/GameTest.java
@@ -0,0 +1,28 @@
+package univ.cnu.lecture.gostop;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by tchi on 2017. 4. 23..
+ */
+public class GameTest {
+    private Game game;
+
+    @Before
+    public void setup() {
+        game = new Game();
+    }
+
+    @Test
+    public void testPutOneCardAndGainNothing() {
+        Player player = game.currentPlayer();
+        Card card = player.nextCard();
+        Card[] gainings = game.putCard(card);
+
+        assertThat(gainings.length, is(0));
+    }
+}
